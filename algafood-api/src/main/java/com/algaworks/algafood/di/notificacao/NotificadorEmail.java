b6646f1d9892a3244c8fc36922acb2adcf166737
alvescamila87 <alvescamila87@gmail.com>
2026-05-13 19:32:24 -0300

    feat: @configuration and @bean

--- algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,17 +1,28 @@
 package com.algaworks.algafood.di.notificacao;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import org.springframework.stereotype.Component;
 
-@Component
 public class NotificadorEmail implements Notificador {
 
-    public NotificadorEmail() {
+    private boolean caixaAlta;
+    private String servidorHostSmtp;
+
+    public NotificadorEmail(String servidorHostSmtp) {
+        this.servidorHostSmtp = servidorHostSmtp;
+
         System.out.println("Chamou construtor NotificacaoEmail");
     }
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.printf("Notificação via email para %s no e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
+        if(caixaAlta) {
+            mensagem = mensagem.toUpperCase();
+        }
+
+        System.out.printf("Notificação via email para %s no e-mail %s usando o SMTP %s: %s\n", cliente.getNome(), cliente.getEmail(), this.servidorHostSmtp, mensagem);
+    }
+
+    public void setCaixaAlta(boolean caixaAlta) {
+        this.caixaAlta = caixaAlta;
     }
 }
