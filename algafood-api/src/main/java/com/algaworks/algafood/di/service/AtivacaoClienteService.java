64229dcce53ecddfc18c3862c8ce59a8c59f0171
alvescamila87 <alvescamila87@gmail.com>
2026-05-20 19:13:10 -0300

    feat @Primary

--- algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -6,25 +6,22 @@ import com.algaworks.algafood.di.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 
 @Component
 public class AtivacaoClienteService {
 
-    private List<Notificador> notificadores;
+    private Notificador notificador;
 
     @Autowired
-    public AtivacaoClienteService(List<Notificador> notificadores) {
-        this.notificadores = notificadores;
-        System.out.println("Chamou construtor AtivacaoClienteService: " + notificadores);
+    public AtivacaoClienteService(Notificador notificador) {
+        this.notificador = notificador;
+        System.out.println("Chamou construtor AtivacaoClienteService: " + notificador);
     }
 
 
     public void ativar(Cliente cliente) {
         cliente.Ativar();
 
-        for (Notificador notificador : notificadores) {
-            notificador.notificar(cliente, "Seu cadastro no sistema foi ativado!");
-        }
+        notificador.notificar(cliente, "Seu cadastro no sistema foi ativado!");
     }
 }
